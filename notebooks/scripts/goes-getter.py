9d83a096a67b4340caa016196222847f1b4efcf6
anissa111 <anissaz@ucar.edu>
2023-11-17 20:25:03 -0700

    get a full day of hourly data instead of 5m data for a few hours

--- notebooks/scripts/goes-getter.py
+++ notebooks/scripts/goes-getter.py
@@ -6,12 +6,14 @@ base_url = 'https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/'
 r = requests.get(base_url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-# get links to 20 images
+# get all image urls
 urls = []
 for link in soup.find_all('a'):
-    if link.get('href').endswith('416x250.jpg') and len(urls) < 30:
+    if link.get('href').endswith('416x250.jpg'):
         urls.append(link.get('href'))
 
+# get a day's worth of hourly images
+urls = urls[::12][:24]
 # download images
 for url in urls:
     im = requests.get(base_url + url).content
